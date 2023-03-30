49622a4b18b5bd13d00b5cd6c55024a756c2066b
rbw9891 <rbw9891@gmail.com>
2023-03-30 16:01:51 -0400

    basic mongo creation, connection, render w/ flask

--- app.py
+++ app.py
@@ -5,15 +5,21 @@
 #   - templates (dir)
 #       > index.html
 from flask import Flask, render_template, url_for
+from flask_pymongo import PyMongo
 
 # create instance of Flask class
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/streamTest"
+mongo = PyMongo(app)
 
 # define how we get to page with app.route
 @app.route("/")
 # what will be displayed on pg wrapped in this function
 def home():
-    return render_template("index.html")
+    #first document in the collection
+    first_record = mongo.db.streamData.find_one()
+    return render_template("index.html", first_record=first_record)
 # after the source file is where the {{variable}} from the html is being set to
 
 # run webpage
