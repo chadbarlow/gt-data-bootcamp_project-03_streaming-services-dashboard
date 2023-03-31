1d3b11bd8b893907a772be450b9065145b217103
rbw9891 <rbw9891@gmail.com>
2023-03-30 22:07:44 -0400

    get_data route, serves mongo data to url as json

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-#import flask class
-# import render_template (allows flask server to read in html for display in browser)
-#need the following structure of folders
-#   - app.py
-#   - templates (dir)
-#       > index.html
 from flask import Flask, render_template, url_for
 from flask_pymongo import PyMongo
+import json
+from bson import json_util
+from bson.json_util import dumps
 import create_mongodb
 
 # create instance of Flask class
@@ -23,6 +20,18 @@ def home():
     first_record = mongo.db.streamData.find_one()
     return render_template("index.html", first_record=first_record)
 # after the source file is where the {{variable}} from the html is being set to
+
+@app.route("/get_data")
+def get_data():
+    # variable to find all data in streamData collection
+    mongo_all = mongo.db.streamData.find()
+    #empty list to be transformed into json object
+    json_all = []
+    for all in mongo_all:
+        json_all.append(all)
+    # converting mongo encoding to json
+    json_all = json.dumps(json_all, default=json_util.default)
+    return json_all
 
 # run webpage
 # set debug to True if you want server to auto reload code changes
